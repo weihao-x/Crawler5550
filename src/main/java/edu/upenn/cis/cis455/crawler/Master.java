378af33f8d7c385d77aae9acd6839f6d146c635e
weihaox <weihaox@windows10.microdone.cn>
2022-05-02 00:46:32 -0400

    1

--- src/main/java/edu/upenn/cis/cis455/crawler/Master.java
+++ src/main/java/edu/upenn/cis/cis455/crawler/Master.java
@@ -52,10 +52,10 @@ public class Master {
 					hash = 7;
 					host = (new URL(url)).getHost();
 					for (int i = 0; i < host.length() ; i++) {
-					    hash = hash*31 + host.charAt(i);
+					    hash = hash*11 + host.charAt(i);
 					}
-					logger.info(url + " send to worker " + workers.get(hash % workers.size()));
-					con = (HttpURLConnection) (new URL("http://" + workers.get(hash % workers.size()) + "/add?url=" + url)).openConnection();
+					logger.info(url + " send to worker " + workers.get(Math.floorMod(hash, workers.size())));
+					con = (HttpURLConnection) (new URL("http://" + workers.get(Math.floorMod(hash, workers.size())) + "/add?url=" + url)).openConnection();
 					con.setRequestMethod("POST");
 					con.setRequestProperty("Content-Type", "application/json");
 					con.getResponseCode();
